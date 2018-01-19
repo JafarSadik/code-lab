8d49317f8d8f8b09556e65e56c76bba801ab7cc0
Jafar Sadik <dzafar.sadik@gmail.com>
2018-01-19 17:31:49 +0000

    coursera introduction to scala - week 1 - basic hello world using scala and sbt

---
+++ scala/coursera-scala-principles/week1/HelloWorld/src/main/scala/example/HelloScala.scala
@@ -0,0 +1,17 @@
+package example
+
+/*
+  Basic hello-world scala example makes use of App trait and therefore
+  explicit declaration of main() method is not necessary
+ */
+object HelloScala extends App {
+  println("Hello Scala!")
+}
+
+/*
+// Explicit declaration of main() method:
+object HelloScala {
+  def main(args: Array[String]): Unit = {
+    println("Hello Scala!")
+  }
+}*/
