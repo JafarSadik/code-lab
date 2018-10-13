08a9a09438afae1b7ff65fb5a4b910507b9d5719
Jafar Sadik <dzafar.sadik@gmail.com>
2018-10-13 22:09:41 +0100

    webdev: es6 classes

---
+++ webdev/es6+/es6-classes.js
@@ -0,0 +1,33 @@
+// Class definition is very concise. Out of many things it supports: constructor, inheritance and static methods.
+class Unit {
+    constructor(id, x, y) {
+        this.id = id;
+        this.move(x, y);
+        this.visible = true;
+    }
+
+    move(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    toString() {
+        return `Unit(${this.id})`;
+    }
+
+    static hidden(id) {
+        return new DisabledUnit(id);
+    }
+}
+
+console.log(new Unit(1, 10, 15));
+
+// Class inheritance
+class DisabledUnit extends Unit {
+    constructor(id) {
+        super(id, 0, 0);
+        this.visible = false;
+    }
+}
+
+console.log('> ' + Unit.hidden(12));
