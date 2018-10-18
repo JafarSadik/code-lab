67eee76630cb18100cfb9cafd6e61c3e045b7bb7
Jafar Sadik <dzafar.sadik@gmail.com>
2018-10-18 08:13:14 +0100

    webdev: es6 every, some

--- webdev/es6+/es6-fp.js
+++ webdev/es6+/es6-fp.js
@@ -13,12 +13,18 @@ equal(21, [0, 1, 2, 3, 4, 5, 6].reduce((sum, value) => sum + value, 0));
 deepEqual([1, 2, 3, 4, 5, 6], [[1], [2, 3, 4], [5, 6]].reduce((array, item) => array.concat(item)), []);
 
 // flatMap() is similar to map but each element can be mapped to multiple elements
-Array.prototype.flatMap = function(f) {
+Array.prototype.flatMap = function (f) {
     return this.reduce((list, value, idx) => list.concat(f(value, idx)), []);
 };
 
 deepEqual(['never', 'underestimate', 'the', 'power', 'of', 'stupid', 'people', 'in', 'large', 'groups'],
     ["never underestimate the power of stupid", "people in large", "groups"].flatMap(e => e.split(" ")));
+
+// every() determines whether all members of an array satisfies the specified test
+ok(['cpu', 'motherboard', 'memory', 'computer'].every(e => e.length > 2));
+
+// some() determines whether the specified callback returns true for any member of an array
+ok([1, 10, 15, 100].some(e => e === 15));
 
 // zip()
 Array.prototype.zip = function (that) {
@@ -30,6 +36,7 @@ Array.prototype.zip = function (that) {
 };
 
 deepEqual([], [].zip([]));
+deepEqual([], [1, 2, 3].zip([]));
+deepEqual([[1, {a: '1', b: '2', c: '3'}]], [1, 2, 3, 5, 6].zip([{a: '1', b: '2', c: '3'}]));
 deepEqual([[1, 3]], [1, 2].zip([3]));
 deepEqual([[1, 'a'], [2, 'b'], [3, 'c'], [4, 'd'], [5, 'e']], [1, 2, 3, 4, 5].zip(['a', 'b', 'c', 'd', 'e']));
-
