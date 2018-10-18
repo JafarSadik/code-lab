d4f25acb8225a14499e9b00bd07db6a584efff47
Jafar Sadik <dzafar.sadik@gmail.com>
2018-10-18 01:10:03 +0100

    webdev: es6 map, filter, reduce, flatMap, zip

---
+++ webdev/es6+/es6-fp.js
@@ -0,0 +1,35 @@
+import "@babel/polyfill"
+import {deepEqual, equal, notEqual, ok} from "assert"
+
+
+// map() creates a new array with the results of calling a provided function on every element
+deepEqual([4, 2, 11], ['this', 'is', 'interesting'].map(word => word.length));
+
+// filter() creates a new array with all elements that pass test implemented by the provided function
+deepEqual([10, 11], [1, 2, 3, 4, 6, 9, 10, 11].filter(e => e >= 10));
+
+// reduce() applies a function against an accumulator and each element in the array to reduce it to a single value
+equal(21, [0, 1, 2, 3, 4, 5, 6].reduce((sum, value) => sum + value, 0));
+deepEqual([1, 2, 3, 4, 5, 6], [[1], [2, 3, 4], [5, 6]].reduce((array, item) => array.concat(item)), []);
+
+// flatMap() is similar to map but each element can be mapped to multiple elements
+Array.prototype.flatMap = function(f) {
+    return this.reduce((list, value, idx) => list.concat(f(value, idx)), []);
+};
+
+deepEqual(['never', 'underestimate', 'the', 'power', 'of', 'stupid', 'people', 'in', 'large', 'groups'],
+    ["never underestimate the power of stupid", "people in large", "groups"].flatMap(e => e.split(" ")));
+
+// zip()
+Array.prototype.zip = function (that) {
+    const length = Math.min(this.length, that.length);
+    return this.flatMap((item, idx) => {
+        if (idx < length) return [[item, that[idx]]];
+        else return [];
+    });
+};
+
+deepEqual([], [].zip([]));
+deepEqual([[1, 3]], [1, 2].zip([3]));
+deepEqual([[1, 'a'], [2, 'b'], [3, 'c'], [4, 'd'], [5, 'e']], [1, 2, 3, 4, 5].zip(['a', 'b', 'c', 'd', 'e']));
+
