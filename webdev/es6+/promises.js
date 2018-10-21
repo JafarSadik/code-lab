2de9df32d5f9a7afdb47b84df9974d1ed4f06387
Jafar Sadik <dzafar.sadik@gmail.com>
2018-10-21 15:31:27 +0100

    webdev: renamed es6+ files

---
+++ webdev/es6+/promises.js
@@ -0,0 +1,4 @@
+// https://flaviocopes.com/javascript-promises/
+
+// Interesting example, recursively processing an array of promises
+//https://hackernoon.com/nodejs-javascript-killer-of-resolving-sequential-promises-pacu-a0a6b9a45045
