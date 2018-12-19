b3f20e65e4cf45ba6343a5a9845d254427e0a38b
Jafar Sadik <dzafar.sadik@gmail.com>
2018-12-19 12:26:30 +0000

    React: logging React component lifecycle callbacks

--- webdev/react/hello-world/src/App.jsx
+++ webdev/react/hello-world/src/App.jsx
@@ -15,6 +15,39 @@ export default class App extends React.Component {
         this.setState({message: newMessage})
     };
 
+    componentWillMount() {
+        console.log(`componentWillMount`);
+    }
+
+    componentDidMount() {
+        console.log(`componentDidMount`);
+    }
+
+    componentWillUnmount() {
+        console.log(`componentWillUnmount`);
+    }
+
+    componentWillUpdate(nextProps, nextState, nextContext) {
+        console.log(`componentWillUpdate`);
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log(`componentDidUpdate`);
+    }
+
+    componentDidCatch(error, errorInfo) {
+        console.log(`componentDidCatch: ${error}`);
+    }
+
+    componentWillReceiveProps(nextProps, nextContext) {
+        console.log(`componentWillReceiveProps`);
+    }
+
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        console.log(`shouldComponentUpdate`);
+        return true;
+    }
+
     render() {
         return <>
             <MessageDisplay active message={this.state.message}/>
